a89abb2f3e16d9906f233f65f014a7d61a7589bb
Tisu <piotrszkudlarski@gmail.com>
2018-07-15 22:28:22 +0200

    Added test for reading enum out of range

---
+++ IniWrapper/IniWrapper.IntegrationTests/Main/Read/WrongFormat/ReadingEnumOutOfRangeTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using IniWrapper.IntegrationTests.Main.Configuration.Properties;
+using IniWrapper.IntegrationTests.MockParser;
+using IniWrapper.ParserWrapper;
+using IniWrapper.Wrapper;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace IniWrapper.IntegrationTests.Main.Read.WrongFormat
+{
+    [TestFixture]
+    public class ReadingEnumOutOfRangeTests
+    {
+        private IIniWrapper _iniWrapper;
+
+        private IIniParser _iniParser;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _iniParser = Substitute.For<IIniParser>();
+            _iniWrapper = MockParserFactory.CreateWithFileSystem(_iniParser);
+        }
+
+        [Test]
+        public void LoadConfiguration_CorrectEnumOutOfRange()
+        {
+            _iniParser.Read(nameof(TestConfiguration), nameof(TestConfiguration.TestEnum)).Returns("1000");
+
+            var result = _iniWrapper.LoadConfiguration<TestConfiguration>();
+
+            result.TestEnum.Should().Be((TestEnum)1000);
+        }
+    }
+}
--- IniWrapper/IniWrapper.IntegrationTests/Main/Read/WrongFormat/ReadingWrongFormattedIniTests.cs
+++ IniWrapper/IniWrapper.IntegrationTests/Main/Read/WrongFormat/ReadingWrongFormattedIniTests.cs
@@ -10,7 +10,7 @@ using NUnit.Framework;
 
 namespace IniWrapper.IntegrationTests.Main.Read.WrongFormat
 {
-    public class ReadingWrongFormattedIni
+    public class ReadingWrongFormattedIniTests
     {
         private IIniWrapper _iniWrapper;
 
